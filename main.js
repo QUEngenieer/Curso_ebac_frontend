61fd323e66baf7c4db43c61c177f91c45e46fa84
Gabriel <gabi.dos96@gmail.com>
2023-06-01 12:39:38 -0300

    Commitando o codigo completo

---
+++ main.js
@@ -0,0 +1,26 @@
+const form = document.getElementById('form-validar');
+
+form.addEventListener('submit', function(evento){
+    evento.preventDefault();
+        const numeroA = parseFloat(document.getElementById('numero-a').value);
+        const numeroB = parseFloat(document.getElementById('numero-b').value);
+        
+        const mensagemSucesso = `Formulário válido!`
+        const containerMensagemSucesso = document.querySelector('.success-message');
+        containerMensagemSucesso.textContent = '';
+        containerMensagemSucesso.style.display = 'none';
+        
+        
+        const mensagemErro = `Formulário inválido!`
+        const containerMensagemErro = document.querySelector('.error-message');
+        containerMensagemErro.textContent = '';
+        containerMensagemErro.style.display = 'none';
+        
+        if(numeroA < numeroB){
+        containerMensagemSucesso.innerHTML = mensagemSucesso;
+        containerMensagemSucesso.style.display = 'block';
+        }else{
+        containerMensagemErro.innerHTML = mensagemErro;
+        containerMensagemErro.style.display = 'block';
+        }
+});
